d676c265af2a9e1a7413f491e7e8bee42e9564dc
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-02-03 14:50:57 +0530

    update home

--- view/home.php
+++ view/home.php
@@ -12,9 +12,19 @@
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-    <button class="mainbtn">Add/Remove Items</button>
-    <button class="mainbtn">Add order</button>
-    <button class="mainbtn">Previous Orders</button>
-    <button class="mainbtn">Amounts</button>
+    <div class="container">
+        <div class="col">
+            <button class="">Add/Remove Items</button>
+        </div>
+        <div class="col">
+            <button class="">Add Order</button>
+        </div>
+        <div class="col">
+            <button class="">Previous Orders</button>
+        </div>
+        <div class="col">
+            <button class="">Amounts</button>
+        </div>
+    </div>
 </body>
 </html>
