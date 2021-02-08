a78b7ce8beceb1b98018fdbdf60f3467164d75b5
Janith Renuka <56946043+janithrenuka@users.noreply.github.com>
2021-02-08 16:20:02 +0530

    create basic home pages

---
+++ view/storekeeper/skHomeV.php
@@ -0,0 +1,17 @@
+<?php
+    require('../basic/topnav.php');
+?>
+
+<main>
+    <title>Home</title>
+
+    
+        <div class="column">
+            <h2>Store Keeper Home Page</h2>
+        </div>
+    </div>
+</main>
+
+<?php
+    require('../basic/footer.php');
+?>
